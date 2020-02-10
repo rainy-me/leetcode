5d57ab7a16763b23920a35c1e6aa8c4b9022f9f8
rainy-me <github@rainy.me>
2020-02-11 03:13:30 +0900

    update

--- api.py
+++ api.py
@@ -2,26 +2,58 @@ import os
 import requests
 from credentials import headers, cookies
 
-response = requests.get(
-    'https://leetcode.com/api/problems/algorithms/',
-    headers=headers,
-    cookies=cookies
-)
 
-levels = ['easy', 'medium', 'hard']
+def get_data():
+    print('geting data...')
+    return requests.get(
+        'https://leetcode.com/api/problems/algorithms/',
+        headers=headers,
+        cookies=cookies
+    ).json()
 
-for level in levels:
-    os.makedirs(level, exist_ok=True)
 
-for q in response.json()['stat_status_pairs']:
-    stat = q['stat']
-    difficulty = levels[q['difficulty']['level'] - 1]
-    name = stat['question__title_slug']
+def gen():
+    response = get_data()
+    levels = ['easy', 'medium', 'hard']
 
-    file_dir = f'{difficulty}/{name}'
-    file_path = f'{file_dir}/{name}.js'
+    for level in levels:
+        os.makedirs(level, exist_ok=True)
 
-    os.makedirs(file_dir, exist_ok=True)
-    if not os.path.exists(file_path):
-        with open(file_path, 'a') as f:
-            f.write('')
+    for q in response['stat_status_pairs']:
+        stat = q['stat']
+        difficulty = levels[q['difficulty']['level'] - 1]
+        name = stat['question__title_slug']
+
+        file_dir = f'{difficulty}/{name}'
+        file_path = f'{file_dir}/{name}.js'
+
+        os.makedirs(file_dir, exist_ok=True)
+        if not os.path.exists(file_path):
+            with open(file_path, 'a') as f:
+                f.write('')
+
+
+def md():
+    response = get_data()
+    solved = response['num_solved']
+    total = response['num_total']
+    easy = response['ac_easy']
+    medium = response['ac_medium']
+    hard = response['ac_hard']
+    with open('./README.md', 'w') as f:
+        f.write(
+            f"""# leetcode problems
+
+{solved} / {total}
+
+|        |     |
+| ------ | --- |
+| easy   | {easy}  |
+| medium | {medium}   |
+| hard   | {hard}   |
+
+"""
+        )
+
+
+md()
