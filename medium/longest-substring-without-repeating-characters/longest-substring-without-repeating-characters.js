2eb65c650ef002e551d36c41a34b74f667e7caca
rainy-me <github@rainy.me>
2020-02-15 02:48:46 +0900

    update

--- medium/longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
+++ medium/longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -3,5 +3,21 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function (s) {
+  const len = s.length
+  const m = new Map()
+  let max = 0;
+  let lastRepeat = -1;
+
+  for (let i = 0; i < len; i++) {
+    const last = m.get(s[i])
+    if (last > lastRepeat) {
+      lastRepeat = last
+    }
+    m.set(s[i], i)
+    if (max < i - lastRepeat) {
+      max = i - lastRepeat
+    }
+  }
+  return max
 };
 module.exports = lengthOfLongestSubstring
--- medium/longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.test.js
+++ medium/longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.test.js
@@ -4,4 +4,11 @@ test('lengthOfLongestSubstring', () => {
   expect(lengthOfLongestSubstring("abcabcbb")).toBe(3);
   expect(lengthOfLongestSubstring("bbbbb")).toBe(1);
   expect(lengthOfLongestSubstring("pwwkew")).toBe(3);
+  expect(lengthOfLongestSubstring("")).toBe(0);
+  expect(lengthOfLongestSubstring("bwf")).toBe(3);
+  expect(lengthOfLongestSubstring("abba")).toBe(2);
+  expect(lengthOfLongestSubstring("aab")).toBe(2);
+  expect(lengthOfLongestSubstring("tmmzuxt")).toBe(5);
+  expect(lengthOfLongestSubstring("biidygcc")).toBe(5);
+  expect(lengthOfLongestSubstring("dvdf")).toBe(3);
 });
