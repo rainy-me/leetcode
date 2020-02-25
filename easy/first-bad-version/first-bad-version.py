9963c617fdab1af7bf7a72d5c84ebc2b6ca18bc3
rainy-me <github@rainy.me>
2020-02-25 10:59:37 +0900

    update

---
+++ easy/first-bad-version/first-bad-version.py
@@ -0,0 +1,23 @@
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return a bool
+
+
+def isBadVersion(version):
+    return version & 1
+
+
+class Solution:
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        l, r = 1, n
+        while l < r:
+            mid = (l+r)//2
+            if isBadVersion(mid):
+                r = mid
+            else:
+                l = mid+1
+        return l
