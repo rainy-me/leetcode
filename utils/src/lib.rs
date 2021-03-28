fe999ce86644340e0941fc252d1e508e555e9bc8
rainy-me <github@rainy.me>
2021-03-28 16:27:33 +0900

    commit missing utils

---
+++ utils/src/lib.rs
@@ -0,0 +1,61 @@
+#![feature(fn_traits)]
+
+#[macro_export]
+macro_rules! vec_of_strings {
+  ($($x:expr),*) => (vec![$($x.to_string()),*]);
+}
+
+#[macro_export]
+macro_rules! mat {
+  (
+    $(
+      [$($y:expr),*]
+    );*
+  ) => (vec![
+    $(
+      vec![$($y),*]
+    ),*
+  ]);
+  (
+    $(
+      [$($y:expr),*]
+    ),*
+  ) => (vec![
+    $(
+      vec![$($y),*]
+    ),*
+  ]);
+}
+
+#[macro_export]
+macro_rules! test {
+  (
+    $fn:ident,
+    $(
+      ($($args:tt)*) => $result:expr
+    );*;
+  ) => {
+    #[test]
+    fn test() {
+      $(
+        assert_eq!(
+          std::ops::Fn::call(&Solution::$fn, ($($args)*)),
+          $result
+        );
+      )*
+    }
+  };
+}
+
+#[macro_export]
+macro_rules! setup {
+    (
+  ) => {
+        use utils::test;
+
+        #[cfg(test)]
+        struct Solution {}
+
+        fn main() {}
+    };
+}
